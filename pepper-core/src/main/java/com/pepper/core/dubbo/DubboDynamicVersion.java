8249aaa1f0c337873dae615ecb71b3a8802b4767
克比 <coby3@qi-cloud.com>
2019-03-09 18:03:32 +0800

    开始dubbo动态版本设置

--- pepper-core/src/main/java/com/pepper/core/dubbo/DubboDynamicVersion.java
+++ pepper-core/src/main/java/com/pepper/core/dubbo/DubboDynamicVersion.java
@@ -1,18 +1,9 @@
 package com.pepper.core.dubbo;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.Map;
+import java.util.Set;
 
-import org.apache.dubbo.config.annotation.Service;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.core.annotation.AnnotationAttributes;
-import org.springframework.core.type.classreading.AnnotationMetadataReadingVisitor;
+import org.reflections.Reflections;
+import org.springframework.stereotype.Controller;
 
 /**
  * 动态设置duboo版本
@@ -20,28 +11,12 @@ import org.springframework.core.type.classreading.AnnotationMetadataReadingVisit
  * @author mrliu
  *
  */
+public class DubboDynamicVersion {
 
-public class DubboDynamicVersion implements BeanFactoryPostProcessor {
-
-	@Override
-	public void postProcessBeanFactory(ConfigurableListableBeanFactory arg0) throws BeansException {
-		String[] beanStr = arg0.getBeanDefinitionNames();
-		for (String beanName : beanStr) {
-			//if(beanName.equals("adminUserServiceImpl")){
-				BeanDefinition beanDefinition = arg0.getBeanDefinition(beanName);
-				try {
-					Method method = beanDefinition.getClass().getMethod("getMetadata");
-					method.setAccessible(true);
-					AnnotationMetadataReadingVisitor object =  (AnnotationMetadataReadingVisitor) method.invoke(beanDefinition);
-					AnnotationAttributes annotationAttributes = object.getAnnotationAttributes("org.apache.dubbo.config.annotation.Service");
-					if(annotationAttributes!=null){
-						annotationAttributes.put("version", "1.1.1");
-					}
-				} catch (Exception e) {
-//					e.printStackTrace();
-				}
-			//}
-		}
+	public DubboDynamicVersion() {
+		super();
+		Reflections reflections = new Reflections("com.sea.server.hessian");
+		Set<Class<?>> hessianImpls = reflections.getTypesAnnotatedWith(Controller.class);
 	}
 
 }
